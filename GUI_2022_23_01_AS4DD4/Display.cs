013ecb996e4dddfe1f7ea506602591b17105d311
barna33 <barnabas.bradali@gmail.com>
2022-11-15 08:27:52 +0100

    Add project files.

---
+++ GUI_2022_23_01_AS4DD4/Display.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Media;
+
+namespace GUI_2022_23_01_AS4DD4
+{
+    internal class Display : FrameworkElement
+    {
+        Size size;
+        Random r = new Random();
+
+        public void Resize(Size size)
+        {
+            this.size = size;
+            InvalidateVisual();
+        }
+
+        protected override void OnRender(DrawingContext drawingContext)
+        {
+            base.OnRender(drawingContext);
+            //Example for testing renderer
+            if (size.Width > 0 && size.Height > 0)
+            {
+                int row = r.Next(10, 30);
+                int column = r.Next(10, 30);
+
+                double rectWidth = size.Width / column;
+                double rectHeight = size.Height / row;
+
+                for (int i = 0; i < row; i++)
+                {
+                    for (int j = 0; j < column; j++)
+                    {
+                        drawingContext.DrawRectangle(
+                            new SolidColorBrush(Color.FromRgb((byte)r.Next(0, 255), (byte)r.Next(0, 255), (byte)r.Next(0, 255))),
+                            new Pen(Brushes.Black, 0),
+                            new Rect(j * rectWidth, i * rectHeight, rectWidth, rectHeight));
+                    }
+                }
+
+            }
+        }
+    }
+}
---
+++ GUI_2022_23_01_AS4DD4/MainWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace GUI_2022_23_01_AS4DD4
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_SizeChanged(object sender, SizeChangedEventArgs e)
+        {
+            if (grid.ActualWidth > 0 && grid.ActualHeight > 0)
+            {
+                display.Resize(new Size()
+                {
+                    Width = grid.ActualWidth,
+                    Height = grid.ActualHeight
+                });
+            }
+
+        }
+    }
+}
