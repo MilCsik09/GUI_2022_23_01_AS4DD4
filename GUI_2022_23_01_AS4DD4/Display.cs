eff688c17464acabee6f64d6dba26f7462b0dfcc
barna33 <barnabas.bradali@gmail.com>
2022-12-05 18:06:46 +0100

    Background added

--- GUI_2022_23_01_AS4DD4/Display.cs
+++ GUI_2022_23_01_AS4DD4/Display.cs
@@ -1,11 +1,13 @@
 ﻿using GUI_2022_23_01_AS4DD4.Logic;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Media;
+using System.Windows.Media.Imaging;
 
 namespace GUI_2022_23_01_AS4DD4
 {
@@ -35,30 +37,39 @@ namespace GUI_2022_23_01_AS4DD4
             InvalidateVisual();
         }
 
+        public Brush BackgroundBrush
+        {
+            get
+            {
+                return new ImageBrush(new BitmapImage(new Uri(Path.Combine("Images", "Background.jpg"), UriKind.RelativeOrAbsolute)));
+            }
+        }
+
         //protected override void OnRender(DrawingContext drawingContext)
         //{
         //    base.OnRender(drawingContext);
+
         //    //Example for testing renderer
-        //    if (size.Width > 0 && size.Height > 0)
-        //    {
-        //        int row = r.Next(10, 30);
-        //        int column = r.Next(10, 30);
+        ////    if (size.Width > 0 && size.Height > 0)
+        ////    {
+        ////        int row = r.Next(10, 30);
+        ////        int column = r.Next(10, 30);
 
-        //        double rectWidth = size.Width / column;
-        //        double rectHeight = size.Height / row;
+        ////        double rectWidth = size.Width / column;
+        ////        double rectHeight = size.Height / row;
 
-        //        for (int i = 0; i < row; i++)
-        //        {
-        //            for (int j = 0; j < column; j++)
-        //            {
-        //                drawingContext.DrawRectangle(
-        //                    new SolidColorBrush(Color.FromRgb((byte)r.Next(0, 255), (byte)r.Next(0, 255), (byte)r.Next(0, 255))),
-        //                    new Pen(Brushes.Black, 0),
-        //                    new Rect(j * rectWidth, i * rectHeight, rectWidth, rectHeight));
-        //            }
-        //        }
+        ////        for (int i = 0; i < row; i++)
+        ////        {
+        ////            for (int j = 0; j < column; j++)
+        ////            {
+        ////                drawingContext.DrawRectangle(
+        ////                    new SolidColorBrush(Color.FromRgb((byte)r.Next(0, 255), (byte)r.Next(0, 255), (byte)r.Next(0, 255))),
+        ////                    new Pen(Brushes.Black, 0),
+        ////                    new Rect(j * rectWidth, i * rectHeight, rectWidth, rectHeight));
+        ////            }
+        ////        }
 
-        //    }
+        ////    }
         //}
     }
 }
